3b68a4dc704d8f7eb45498ca6409c77082f2bee7
Fajar Nugroho <fajar12.nugroho@gmail.com>
2018-11-11 23:54:49 +0700

    add models and ORM framework

---
+++ models/Transaction.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, type) => {
+  return sequelize.define('transaction', {
+    transaction_id: {
+      type: type.UUID,
+      defaultValue: type.UUIDV1,
+      primaryKey: true
+    },
+    bill_id: {
+      type: type.UUID,
+      allowNull: false,
+      references: {
+        model: 'bills',
+        key: 'bill_id'
+      }
+    },
+    customer_id: {
+      type: type.UUID,
+      allowNull: false,
+      references: {
+        model: 'customers',
+        key: 'customer_id'
+      }
+    },
+    merchant_id: {
+      type: type.UUID,
+      allowNull: false,
+      references: {
+        model: 'merchants',
+        key: 'merchant_id'
+      },
+    },
+    total: {
+      type: type.INTEGER,
+      allowNull: false
+    },
+    detail: type.STRING(128),
+    time: {
+      type: type.DATE,
+      defaultValue: type.NOW
+    }
+  });
+};
